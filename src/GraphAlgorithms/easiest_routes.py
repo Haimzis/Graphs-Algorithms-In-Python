1ac53fa251f8a2a29f4cc3a19c7fcc604b3e027b
Haim Zisman <haimzismann@gmail.com>
2019-10-22 02:15:39 +0300

    Topological sort

--- src/GraphAlgorithms/easiest_routes.py
+++ src/GraphAlgorithms/easiest_routes.py
@@ -8,3 +8,10 @@ def bellman_ford(graph, start_vertex):
 
 def directed_acyclic(graph, start_vertex):
     pass
+
+
+def relax(vertex_u,vertex_v,delta_weights, edges_weights, parents):
+    if delta_weights[vertex_v] > delta_weights[vertex_u] + edges_weights[vertex_u, vertex_v]:
+        delta_weights[vertex_v] = delta_weights[vertex_u] + edges_weights[vertex_u, vertex_v]
+        if parents is not None:
+            parents[vertex_v] = vertex_u
