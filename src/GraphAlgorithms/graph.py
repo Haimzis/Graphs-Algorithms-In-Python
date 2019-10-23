c085fed85810a94d6211b6b27f44815c7bb20c81
Haim Zisman <haimzismann@gmail.com>
2019-10-24 02:02:26 +0300

    Done. just need to fix the dijkstra

---
+++ src/GraphAlgorithms/graph.py
@@ -0,0 +1,140 @@
+from collections import defaultdict
+from sys import maxsize
+import queue
+
+
+class Graph:
+    def __init__(self, adjacency_list, edges_weights):
+        self.adjacency_list = adjacency_list
+        self.weights = edges_weights
+
+    def __str__(self):
+        graph_string = "The vertex of the graph are:\n"
+        for counter, vertex in enumerate(self.adjacency_list.keys()):
+            graph_string += f"{counter + 1}.{vertex}\n"
+        graph_string += "\nThe edges of the graph are:\n| "
+        for vertex in self.adjacency_list:
+            for adj in self.adjacency_list[vertex]:
+                graph_string += f"({vertex},{adj}) | "
+        return graph_string
+
+    def add_vertex(self, *args):
+        for vertex in args:
+            self.adjacency_list[vertex] = []
+
+    def add_edge(self, edge, weight=0):
+        vertex_u, vertex_v = edge
+        self.adjacency_list[vertex_u].append(vertex_v)
+        self.weights[edge] = weight
+
+    # Topological Sort
+    def topological_sorting(self):
+        top_sort = ()
+        sources_queue = queue.Queue()
+        in_degrees_counter = self.in_degrees()
+        for vertex, counter in in_degrees_counter.items():
+            if counter == 0:
+                sources_queue.queue.append(vertex)
+        while not sources_queue.empty():
+            vertex = sources_queue.queue.popleft()
+            top_sort += (vertex,)
+            for adjacent in self.adjacency_list[vertex]:
+                in_degrees_counter[adjacent] -= 1
+                if in_degrees_counter[adjacent] == 0:
+                    sources_queue.queue.append(adjacent)
+        for vertex, counter in in_degrees_counter.items():
+            if counter != 0:
+                return tuple(("Failure", None))
+        return tuple(("Success", top_sort))
+
+    def in_degrees(self):
+        in_degree_vertexes = {vertex: 0 for (vertex, adjacency_list) in self.adjacency_list.items()}
+        for vertex in self.adjacency_list:
+            for adjacent in self.adjacency_list[vertex]:
+                in_degree_vertexes[adjacent] += 1
+        return in_degree_vertexes
+
+    # Scans
+    def dfs(self):
+        dfs_order = []
+        colors = {vertex: "white" for vertex in self.adjacency_list.keys()}
+        for vertex in self.adjacency_list.keys():
+            if colors[vertex] == "white":
+                self.visit(vertex, colors, dfs_order)
+        return tuple(dfs_order)
+
+    def visit(self, vertex_u, colors, dfs_order):
+        colors[vertex_u] = "gray"
+        for adjacent in self.adjacency_list[vertex_u]:
+            if colors[adjacent] == "white":
+                self.visit(adjacent, colors, dfs_order)
+        colors[vertex_u] = "black"
+        dfs_order.append(vertex_u)
+
+    def bfs(self, source_vertex):
+        delta_routes_length = {vertex: "infinite" for vertex in self.adjacency_list.keys()}
+        vertexes_queue = queue.Queue()
+        vertexes_queue.queue.append(source_vertex)
+        delta_routes_length[source_vertex] = 0
+        while not vertexes_queue.empty():
+            vertex_u = vertexes_queue.queue.popleft()
+            for adjacent in self.adjacency_list[vertex_u]:
+                if delta_routes_length[adjacent] == "infinite":
+                    delta_routes_length[adjacent] = delta_routes_length[vertex_u] + 1
+                    vertexes_queue.queue.append(adjacent)
+        return delta_routes_length
+
+    # Shortest Paths
+    def dijkstra(self, source_vertex):
+        if min(dict(self.weights).values()) < 0:
+            return tuple(("Failure", None, None))
+        p_queue = queue.PriorityQueue()
+        delta_weights = defaultdict(lambda: maxsize)
+        parents_tree = defaultdict(lambda: None)
+        self.initialize_single_source(source_vertex, delta_weights, parents_tree)
+        p_queue.put((0, source_vertex))
+        while not p_queue.empty():
+            vertex_u = p_queue.get()[1]
+            for vertex_v in self.adjacency_list[vertex_u]:
+                self.relax(vertex_u, vertex_v, delta_weights, parents_tree)
+        return tuple(("Success", delta_weights, parents_tree))
+
+    def bellman_ford(self, source_vertex):
+        edges_list = self.get_edge_list()
+        delta_weights = defaultdict(lambda: maxsize)
+        parents_tree = defaultdict(lambda: None)
+        self.initialize_single_source(source_vertex, delta_weights, parents_tree)
+        for _ in range(1, self.adjacency_list.keys().__len__() - 1):
+            for (vertex_u, vertex_v) in edges_list:
+                self.relax(vertex_u, vertex_v, delta_weights, parents_tree)
+        for (vertex_u, vertex_v) in edges_list:
+            if delta_weights[vertex_v] > delta_weights[vertex_u] + self.weights[(vertex_u, vertex_v)]:
+                return tuple(("Failure", None, None))
+        return tuple(("Success", delta_weights, parents_tree))
+
+    def directed_acyclic(self, source_vertex):
+        delta_weights = defaultdict(lambda: maxsize)
+        parents_tree = defaultdict(lambda: None)
+        self.initialize_single_source(source_vertex, delta_weights, parents_tree)
+        topological_sorted_list = self.topological_sorting()
+        if topological_sorted_list[0] == "Failure":
+            return tuple(("Failure", None, None))
+        for vertex_u in topological_sorted_list[1]:
+            for vertex_v in self.adjacency_list[vertex_u]:
+                self.relax(vertex_u, vertex_v, delta_weights, parents_tree)
+        return tuple(("Success", delta_weights, parents_tree))
+
+    def relax(self, vertex_u, vertex_v, delta_weights, parents):
+        if delta_weights[vertex_v] > delta_weights[vertex_u] + self.weights[(vertex_u, vertex_v)]:
+            delta_weights[vertex_v] = delta_weights[vertex_u] + self.weights[(vertex_u, vertex_v)]
+            parents[vertex_v] = vertex_u
+
+    def initialize_single_source(self, source_vertex, delta_weights, parents_tree):
+        for vertex in self.adjacency_list.keys():
+            delta_weights[vertex] = maxsize
+            parents_tree[vertex] = None
+        delta_weights[source_vertex] = 0
+
+    def get_edge_list(self):
+        return [(vertex_v, vertex_u) for vertex_v in self.adjacency_list.keys() for vertex_u in
+                self.adjacency_list[vertex_v]]
--- src/my_program.py
+++ src/my_program.py
@@ -1,71 +1,13 @@
-from GraphAlgorithms import sorts, searches, easiest_routes
-from collections import defaultdict
-
-
-def take_input():
-    user_input = ''
-    while True:
-        user_input = input("would you like to continue? [Y - yes / N - No]\n")
-        if user_input.lower() == 'n' or user_input.lower() == 'y':
-            break
-        else:
-            print("wrong input!")
-            continue
-    return user_input
-
-
-def print_graph(graph):
-    print("The vertex of the graph are:")
-    for counter, vertex in enumerate(graph.keys()):
-        print(f"{counter+1}. {vertex}:")
-        for adj in graph[vertex]:
-            print(f"({vertex},{adj})")
-
-
-def create_graph():
-    end_of_input = 'y'
-    graph = {}
-    print("Enter Vertexes:")
-    counter = 0
-    while end_of_input == 'y':
-        new_vertex = input(f"vertex number {counter}\n")
-        add_vertex(graph, new_vertex)
-        end_of_input = take_input()
-        counter += 1
-    end_of_input = 'y'
-    counter = 0
-    while end_of_input == 'y':
-        print(f"edge number {counter}, enter (vertex,adjacent)")
-        a = input("enter vertex\n")
-        b = input("enter adjacent\n")
-        add_edge(graph, a, b)
-        end_of_input = take_input()
-        counter += 1
-    return graph
-
-
-def add_weights(graph):
-    weights = defaultdict(lambda: 0)
-    for edge in easiest_routes.get_edge_list(graph):
-        edge_weight = int(input(f"enter weight of {edge}:\n"))
-        weights[edge] = edge_weight
-    return weights
-
-
-def add_edge(graph, vertex, adjacent):
-    graph[vertex].append(adjacent)
-
-
-def add_vertex(graph, new_vertex):
-    graph[new_vertex] = []
-
+from GraphAlgorithms import graph
 
 if __name__ == '__main__':
-    my_graph = create_graph()
-    my_weights = add_weights(my_graph)
-    print_graph(my_graph)
-    print(sorts.topological_sorting(my_graph))
-    print(searches.dfs(my_graph))
-    print(searches.bfs(my_graph, 'a'))
-    print(easiest_routes.bellman_ford(my_graph, my_weights, 'b'))
-    print(easiest_routes.directed_acyclic(my_graph, my_weights, 'b'))
+
+    my_graph = graph.Graph({"a": ["d", "f", "b"], "b": ["c"], "c": ["d", "e"], "d": [], "e": [], "f": []},
+                           {('a', 'b'): 1, ('a', 'd'): 15, ('a', 'f'): 2, ('b', 'c'): 5, ('c', 'd'): 10, ('c', 'e'): 5})
+    print(my_graph.bfs('a'))
+    print(my_graph.dfs())
+    print(my_graph.topological_sorting())
+    print(my_graph.dijkstra('a'))
+    print(my_graph.directed_acyclic('a'))
+    print(my_graph.bellman_ford('a'))
+    print(my_graph.__str__())
